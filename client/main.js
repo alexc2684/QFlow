1dd27c7355e9b36b749ee359b7098f5b8fe31f55
Alex Chan <alex.chan@berkeley.edu>
2016-09-20 23:59:21 -0700

    router fixes, queue updates

--- client/main.js
+++ client/main.js
@@ -3,16 +3,16 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-// var SC = require('soundcloud');
-//
-// SC.initialize({
-//   client_id: 'e49030affa62a819457ef0404ebee1b6';
-// });
-//
-// var track_url = 'https://soundcloud.com/alex-chan-27/kanye-west-mercy';
-// SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
-//   console.log('oEmbed response: ', oEmbed);
-// });
+var SC = require('soundcloud');
+
+SC.initialize({
+  client_id: 'e49030affa62a819457ef0404ebee1b6';
+});
+
+var track_url = 'https://soundcloud.com/alex-chan-27/kanye-west-mercy';
+SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+  console.log('oEmbed response: ', oEmbed);
+});
 
 Template.Home.events({
   'click #newroom': function() {
